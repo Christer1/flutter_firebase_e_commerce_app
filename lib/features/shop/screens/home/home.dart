fbd63e8b3a057e0dce9d705600088bd69754d8ba
Christer1 <yussupholawale2017@gmail.com>
2024-10-11 14:11:37 +0100

    made changes

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -4,35 +4,37 @@ import 'package:e_commerce_app/common/widgets/texts/section_heading.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widget/home_appbar.dart';
 import 'package:e_commerce_app/features/shop/screens/home/widget/home_categories.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
+import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TPrimaryHeaderContainer(
+            const TPrimaryHeaderContainer(
               child: Column(
                 children: [
 
                   //Appbar 
-                  const THomeAppBar(),
-                  const SizedBox(height: TSizes.spaceBtwSections),
+                  THomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
 
                   //Searchbar
-                  const TSearchContainer(text: 'Search in Store'),
-                  const SizedBox(height: TSizes.spaceBtwSections),
+                  TSearchContainer(text: 'Search in Store'),
+                  SizedBox(height: TSizes.spaceBtwSections),
 
                   //Headings
                   Padding(padding: const EdgeInsets.only(left: TSizes.defaultSpace),
                   child: Column(
                     children: [
                       TSectionHeading(title: 'Popular Categories', showActionButton: false, textColor: TColors.white ,),
-                      const SizedBox(height: TSizes.spaceBtwItems),
+                      SizedBox(height: TSizes.spaceBtwItems),
 
                       //Categories
                       THomeCategories(),
@@ -44,9 +46,48 @@ class HomeScreen extends StatelessWidget {
               ),
 
               //body
-              
+              Padding(
+                padding: const EdgeInsets.all(TSizes.defaultSpace),
+                child: TRoundedImage(imageUrl: TImages.productImage1),
+              )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TRoundedImage extends StatelessWidget {
+  const TRoundedImage({
+    super.key, this.width = 150, this.height = 150, required this.imageUrl,  this.applyImageRadius = false, this.border, this.backgroundColor = TColors.light, this.fit = BoxFit.contain, this.padding,  this.isNetWorkImage = false, this.onPressed,  this.borderRadius = TSizes.md,
+  });
+
+  final double? width, height;
+  final String imageUrl;
+  final bool applyImageRadius;
+  final BoxBorder? border;
+  final Color backgroundColor;
+  final BoxFit fit;
+  final EdgeInsetsGeometry? padding;
+  final bool isNetWorkImage;
+  final VoidCallback? onPressed;
+  final double borderRadius;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: width,
+        height: height,
+        padding: padding,
+        decoration: BoxDecoration(
+          border: border,
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+        child: ClipRRect(borderRadius: applyImageRadius ? BorderRadius.circular(borderRadius) : BorderRadius.zero , child: Image(image: isNetWorkImage ? NetworkImage(imageUrl) : AssetImage(imageUrl) as ImageProvider, fit: fit,)),
       ),
     );
   }
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -119,4 +119,22 @@ class TImages{
   static const String productImage76 = "assets/images/products/slipper-product-3.png";
   static const String productImage77= "assets/images/products/slipper-product.png";
 
+
+  // -- Product Reviews
+  static const String userProfileImage1 = "assets/images/reviews/review_profile_image_1.jpg";
+  static const String userProfileImage2 = "assets/images/reviews/review_profile_image_2.jpeg";
+  static const String userProfileImage3 = "assets/images/reviews/review_profile_image_3.jpeg";
+
+  //Banners
+  static const String promoBanner1 = "assets/images/products/promo-banner-1.png";
+  static const String promoBanner2 = "assets/images/products/promo-banner-2.png";
+  static const String promoBanner3 = "assets/images/products/promo-banner-3.png";
+  static const String banner1 = "assets/images/banners/banner_1.jpg";
+  static const String banner2 = "assets/images/banners/banner_2.jpg";
+  static const String banner3 = "assets/images/banners/banner_3.jpg";
+  static const String banner4 = "assets/images/banners/banner_4.jpg";
+  static const String banner5 = "assets/images/banners/banner_5.jpg";
+  static const String banner6 = "assets/images/banners/banner_6.jpg";
+  static const String banner7 = "assets/images/banners/banner_7.jpg";
+  static const String banner8 = "assets/images/banners/banner_8.jpg";
 }
